78c4af45595898fa024bdfee158c3304c8aa2459
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-09 18:39:30 -0400

    Refactor [ move const "Titulo" for components ]

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -3,12 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
 display: flex;
 flex-direction: column;
-align-items: center;
 height: 100vh;
-
-
 `
 
-export const Titulo = styled.h1`
-color: violet;
-`
