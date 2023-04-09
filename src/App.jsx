7b48212d933b9c473ace4de6e5c137c9aa3291b9
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-09 03:13:39 -0400

    fix [ delete style global because styled components issues ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import './global/style.scss'
 import Home from './pages/home'
-import { Button } from './pages/home/style'
 
 function App() {
   return (
     <div className="App">
       <Home/>
-      <Button />
     </div>
   )
 }
