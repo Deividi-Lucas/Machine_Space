4a376d9a7f8134ce4f5914fb951f5b22bcebab8d
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-09 01:44:45 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from './pages/home'
+import { Button } from './pages/home/style'
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Button />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+function Home(){
+    return<>
+    <h1>Home page funcionando
+
+    </h1>
+    </>
+}
+
+export default Home
