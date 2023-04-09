031ac0ef7b6189d84d2ed461fba955b6a7bc35ad
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-09 03:15:10 -0400

    Chore [ home pag for consume api ]

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,22 @@
-function Home(){
-    return<>
-    <h1>Home page funcionando
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import {  Container, Titulo } from './style';
 
-    </h1>
-    </>
+function Home(){
+const [dados, setDados] = useState()
+// Falta consumir a API por via do Axios 
+useEffect(()=>{
+       const get = axios.get("https://api.spacexdata.com/v5/launches/latest")
+        console.log(get.then(res =>  setDados(res.data)))
+
+    },[setDados])
+
+    return(
+    <Container>
+        <Titulo>Machine - Space</Titulo>
+    <p>Aqui você entra dados lançados pela SpaceX </p>
+    </Container>
+    )
 }
 
 export default Home
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,7 +1,14 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
-    padding:1rem ;
-    
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 100vh;
 
+
+`
+
+export const Titulo = styled.h1`
+color: violet;
 `
