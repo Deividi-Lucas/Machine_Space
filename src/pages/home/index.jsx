826ffe933e9f55ed3e44f6b775b0a520e310af03
Deividi-Lucas <deividibaby6@gmail.com>
2023-04-11 17:43:24 -0400

    Feat [ Consume api and show in screen, styled ] <project finished>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,25 +1,49 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import { Container } from './style';
+import { Container, Main, Titulo } from './style';
 import Header from '../../components/header';
+import Footer from '../../components/footer';
 
 
 function Home(){
 const [Hour, setHour] = useState()
 const [img, setImg] = useState()
+const [video, setVideo] = useState()
+const [wiki, setWiki] = useState()
+const [name, setName] = useState()
+const [numberfly, setNumberfly]=useState()
 useEffect(()=>{
     const get = axios.get("https://api.spacexdata.com/v5/launches/latest").then((res)=> {return res})
     // get img in api
     get.then(res=> setImg(res.data.links.patch.small))
     // get hour last flight
     get.then(res=> setHour(res.data.date_local))
+    // get link video 
+    get.then(res=> setVideo(res.data.links.webcast))
+    //get wikepedia link 
+    get.then(res=> setWiki(res.data.links.wikipedia))
+    //get name misson 
+    get.then(res=> setName(res.data.name))
+    // get number fly 
+    get.then(res=> setNumberfly(res.data.flight_number))
+
 
     },[])
 
     return(
     <Container>
-        <Header img={img} alt='Logo falcon 9' titulo='Machine - Space'/>
-        <p>Aqui você entra dados lançados pela SpaceX <span>Horário do último lançamento {Hour}</span> </p>
+        <Header img={img} alt='Logo falcon 9' titulo='Machine Space'/>
+        <Main>
+            <div>
+                <Titulo>{name}</Titulo>
+                <span>Aqui você entra dados lançados pela SpaceX  </span>
+                <span>Horário do último lançamento {Hour}</span>
+                <span>Link do Vídeo <a href={video}>{video}</a></span>
+                <span>URL da Wikipedia <a href={wiki}>{wiki}</a></span>
+                <span>Numero de voo: {numberfly}</span>
+            </div>
+        </Main>
+        <Footer/>
     </Container>
     )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,8 +1,39 @@
 import styled from "styled-components";
+import { color } from "../../utils/color";
 
 export const Container = styled.div`
 display: flex;
 flex-direction: column;
 height: 100vh;
+background: linear-gradient(${color.cinza},${color.blue});
+overflow-y: scroll;
+
+@media (max-width: 290px) {
+    
+    div span{
+        font-size: 12px;
+    }
+    
+}
+`
+export const Main =styled.main`
+height: 100%;
+display: flex;
+flex-direction: column;
+
+div{
+    background-color: rgb(0,0,0,0.5);
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    padding: 3rem;
+    border-radius: 8rem 4rem;
+    span{
+        color: white;
+    }
+}
 `
 
+export const Titulo = styled.h1`
+    color: ${color.black};
+`
